99794379699e3b21f55b785887d2e40fe4dfe42a
Scott Wang (lunastorm) <lunastorm@lunastorm.tw>
2014-04-04 13:46:30 +0800

    move page model to a seperate file and adjust i18n wording

--- example/resources/slide.go
+++ example/resources/slide.go
@@ -5,32 +5,10 @@ import (
     "fmt"
     "time"
     "strconv"
-    "strings"
     "net/http"
     "encoding/json"
-    "html/template"
     "github.com/lunastorm/vitali"
 )
-
-type PageModel struct {
-    Raw string `json:"raw"`
-}
-
-func (c *PageModel) HTML() (res template.HTML) {
-    lines := strings.Split(c.Raw, "\n")
-    for _, line := range(lines) {
-        if line == "" {continue}
-        switch line[0] {
-        case '!':
-            res += template.HTML(fmt.Sprintf("<h1>%s</h1>\n", line[1:]))
-        case '[':
-            res += template.HTML(fmt.Sprintf(`<img src="/image/%s"/>`, line[1:]))
-        default:
-            res += template.HTML(fmt.Sprintf("<h3>%s</h3>\n", line))
-        }
-    }
-    return
-}
 
 type SlideModel struct {
     Pages []PageModel `json:"pages"`
