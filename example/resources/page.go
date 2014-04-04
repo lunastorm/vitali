33a647126ac28aa535b589fc99ae216f52f84bd9
Scott Wang (lunastorm) <lunastorm@lunastorm.tw>
2014-04-04 14:41:08 +0800

    add code display

--- example/resources/page.go
+++ example/resources/page.go
@@ -12,16 +12,31 @@ type PageModel struct {
 
 func (c *PageModel) HTML() (res template.HTML) {
     lines := strings.Split(c.Raw, "\n")
+    var code bool
+    var lineCnt int
     for _, line := range(lines) {
+        if code {
+            lineCnt++
+            res += template.HTML(fmt.Sprintf("%d.\t%s\n", lineCnt, line))
+            continue
+        }
         if line == "" {continue}
         switch line[0] {
         case '!':
             res += template.HTML(fmt.Sprintf("<h1>%s</h1>\n", line[1:]))
         case '[':
             res += template.HTML(fmt.Sprintf(`<img src="/image/%s"/>`, line[1:]))
+        case '*':
+            res += template.HTML(fmt.Sprintf("<li>%s</li>\n", line[1:]))
+        case '{':
+            code = true
+            res += template.HTML("<pre>")
         default:
             res += template.HTML(fmt.Sprintf("<h3>%s</h3>\n", line))
         }
+    }
+    if code {
+        res += template.HTML("</pre>")
     }
     return
 }
