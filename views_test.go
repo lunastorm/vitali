6481c0150237e927d91f2e372ffcd5685cc12ba0
Scott Wang (lunastorm) <lunastorm@lunastorm.tw>
2014-05-18 15:07:18 +0800

    add i18n test

--- views_test.go
+++ views_test.go
@@ -7,6 +7,61 @@ import (
     "net/http/httptest"
 )
 
+type I18nTest struct {
+    Ctx
+    Provides `GET:"text/html"`
+    Views `GET:"i18n_test.html"`
+}
+
+func (c *I18nTest) Get() interface{} {
+    return ""
+}
+
+type TestLangProvider struct {
+}
+
+func (c *TestLangProvider) Select(ctx *Ctx) string {
+    return ctx.Header("Accept-Language")
+}
+
+func TestI18n(t *testing.T) {
+    r := &http.Request{
+        Method: "GET",
+        Host:   "lunastorm.tw",
+        URL: &url.URL{
+            Path: "/i18n",
+        },
+        Header: make(http.Header),
+    }
+    r.Header.Set("Accept-Language", "zh-tw")
+
+    rr := httptest.NewRecorder()
+    webapp := CreateWebApp([]RouteRule{
+        {"/i18n", I18nTest{}},
+    })
+    webapp.LangProvider = &TestLangProvider{}
+    webapp.ServeHTTP(rr, r)
+
+    if rr.Code != http.StatusOK {
+        t.Errorf("response code is %d", rr.Code)
+    }
+    entity := rr.Body.String()
+    if entity != "嗨\n" {
+        t.Errorf("entity is `%s`", entity)
+    }
+
+    r.Header.Set("Accept-Language", "en-us")
+    rr = httptest.NewRecorder()
+    webapp.ServeHTTP(rr, r)
+    if rr.Code != http.StatusOK {
+        t.Errorf("response code is %d", rr.Code)
+    }
+    entity = rr.Body.String()
+    if entity != "hi\n" {
+        t.Errorf("entity is `%s`", entity)
+    }
+}
+
 type View1 struct {
     Ctx
     Provides `GET:"text/html" POST:"text/html"`
